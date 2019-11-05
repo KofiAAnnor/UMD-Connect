c0d075dba0a88a65cd3bf1e51063e5c742b0c208
ichan12 <ichan12@terpmail.umd.edu>
2019-11-05 18:43:07 -0500

    update profile done

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flaskapp.models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Optional
 
 
 class RegistrationForm(FlaskForm):
@@ -31,3 +31,25 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateForm(FlaskForm):
+    username = StringField('New Username',
+                           validators=[Length(min=2,max=20),Optional()])
+    new_email = StringField('New Email',
+                        validators=[Email(),Optional()])
+    skills = StringField('Skills',
+                        validators=[Length(max=500),Optional()])
+    new_password = PasswordField('New Password')
+    confirm_new_password = PasswordField('Confirm New Password',
+                                     validators=[EqualTo('new_password')])
+    old_password = PasswordField('Enter Password to Update',validators=[DataRequired()])
+    submit = SubmitField('Update')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
