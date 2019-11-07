e1e66375dc67eb00835f8f96401a032cb2d29e68
Ayyub A <ayyubb05@gmail.com>
2019-11-06 22:41:07 -0500

    Update models.py

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -10,13 +10,12 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), unique=True, nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    image_file = db.Column(db.String(32), nullable=False, default='default.jpg')
-    password = db.Column(db.String(64), nullable=False)
-    skills = db.Column(db.String(64))
-    description = db.Column(db.String(128))
-
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    skills=db.Column(db.String(500), nullable=True)
+    description = db.Column(db.String(256), nullable=True)
     projects = db.relationship('Project', backref='author', lazy=True)
 
     def __repr__(self):
@@ -25,10 +24,55 @@ class User(db.Model, UserMixin):
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Project('{self.title}', '{self.date_posted}', '{self.content}')"
+        return f"Project('{self.title}', '{self.date_posted}', '{self.id}', '{self.user_id}')"
+
+#   THESE TABLES ARE USED TO STORE THE PROJECTS AND USERS THAT CORRESPOND TO A CERTAIN TAG
+#   THE PREDEFINED TAGS ARE Business, Literature, Technology, Art, Music
+#   THE TITLE COLUMN IS EITHER "PROJECT" OR "USER" AND THE ID COLUMN IS THE ID OF THE PROJECT/USER
+#   THESE TABLES CAN COME IN HANDY WHEN SEARCHING FOR PROJECTS/USERS BASED ON TAGS
+
+
+class Business(db.Model):
+    type = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Business('{self.type}', '{self.name}')"
+
+
+class Literature(db.Model):
+    type = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Literature('{self.type}', '{self.name}')"
+
+
+class Technology(db.Model):
+    type = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Technology('{self.type}', '{self.name}')"
+
+
+class Art(db.Model):
+    type = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Art('{self.type}', '{self.name}')"
+
+
+class Music(db.Model):
+    type = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Music('{self.type}', '{self.name}')"
